de5c64af6742ba8bb1c432489bf325b748313d42
Chirag <patilchirag0430@gmail.com>
2024-09-26 11:19:52 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
 const Discord = require('discord.js');
 const https = require('https');
+require('dotenv').config();
 
-const client = new Discord.Client();
+// Add necessary intents here
+const { GatewayIntentBits } = require('discord.js');
+
+const client = new Discord.Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
 
 const apiUrl = "https://v2.jokeapi.dev/joke/Programming?safe-mode";
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log(`Log In Successful, Client is ${client.user.tag}`);
 });
 
-client.on('message', msg => {
-    if(msg.content ==="joke" || msg.content ==="funny"){
-        fetchJoke((errr, randomJoke) =>{
-            if(errr) {
-                msg.reply("Try again");
-            }else{
-                msg.reply(randomJoke);
-            }
-            
-        });
-    }
-});
-
-function fetchJoke(cb){
-    https.get(apiUrl, response =>{
-        response.on("data", chunk => {
-            let joke = JSON.parse(chunk.toString());
-            if(joke.type === "twopart"){
-                console.log("setup: ", joke.setup);
-                console.log("delivery: ", joke.delivery);
-                return cb(null, `\n Coder1: ${joke.setup} \n Coder2: ${joke.delivery} \n`)
-            }
-            if(joke.type === "single"){
-                console.log("joke: ", joke.joke);
-                return cb(null, `\n Coder: ${joke.joke} \n`)
-            }
-        });
-        response.on("error", err => {
-            console.log("error: ", err);
-            return cb(err);
-        });
+client.on("messageCreate", msg => {  // Changed from "message" to "messageCreate" for Discord.js v13+
+  if (msg.content === "joke" || msg.content === "funny") {
+    fetchJoke((err, randomJoke) => {
+      if (err) {
+        msg.reply(`Try Again Later Alligator!`);
+      } else {
+        msg.reply(randomJoke);
+      }
     });
+  }
+});
+
+function fetchJoke(cb) {
+  https.get(apiUrl, response => {
+    let data = '';
+    response.on("data", chunk => {
+      data += chunk;
+    });
+    response.on("end", () => {
+      let joke = JSON.parse(data);
+      if (joke.type === "twopart") {
+        console.log("setup: ", joke.setup);
+        console.log("delivery: ", joke.delivery);
+        return cb(null, `Test\n Coder1: ${joke.setup} \n Coder2: ${joke.delivery}`);
+      }
+      if (joke.type === "single") {
+        console.log("joke: ", joke.joke);
+        return cb(null, "Test\n" + joke.joke);
+      }
+    });
+
+    response.on("error", err => {
+      console.error(err);
+      return cb(err);
+    });
+  });
 }
 
 client.login(process.env.TOKEN);
